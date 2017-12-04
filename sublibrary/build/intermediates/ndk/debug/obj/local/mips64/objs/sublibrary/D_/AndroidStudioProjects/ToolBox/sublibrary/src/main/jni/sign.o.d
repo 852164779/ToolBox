5e9c75627f6a7c61c7858cc9d7b5155fd282da72
韩望林 <852164779@qq.com>
2017-12-04 14:06:32 +0800

    -a

--- sublibrary/build/intermediates/ndk/debug/obj/local/mips64/objs/sublibrary/D_/AndroidStudioProjects/ToolBox/sublibrary/src/main/jni/sign.o.d
+++
@@ -1,5 +0,0 @@
-D:\AndroidStudioProjects\ToolBox\sublibrary\build\intermediates\ndk\debug\obj/local/mips64/objs/sublibrary/D_\AndroidStudioProjects\ToolBox\sublibrary\src\main\jni\sign.o: \
-  D:\AndroidStudioProjects\ToolBox\sublibrary\src\main\jni\sign.cpp \
-  D:\AndroidStudioProjects\ToolBox\sublibrary\src\main\jni/com_xxm_sublibrary_jni_Ja.h
-
-D:\AndroidStudioProjects\ToolBox\sublibrary\src\main\jni/com_xxm_sublibrary_jni_Ja.h:
--- sublibrary/build/intermediates/ndk/release/obj/local/armeabi-v7a/objs/sublibrary/D_/AndroidStudioProjects/ToolBox/sublibrary/src/main/jni/sign.o.d
+++
@@ -1,5 +0,0 @@
-D:\AndroidStudioProjects\ToolBox\sublibrary\build\intermediates\ndk\release\obj/local/armeabi-v7a/objs/sublibrary/D_\AndroidStudioProjects\ToolBox\sublibrary\src\main\jni\sign.o: \
-  D:\AndroidStudioProjects\ToolBox\sublibrary\src\main\jni\sign.cpp \
-  D:\AndroidStudioProjects\ToolBox\sublibrary\src\main\jni/com_xxm_sublibrary_jni_Ja.h
-
-D:\AndroidStudioProjects\ToolBox\sublibrary\src\main\jni/com_xxm_sublibrary_jni_Ja.h:
